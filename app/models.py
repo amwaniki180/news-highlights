c60d85db16534b2d5c056a4b46480764c907c54f
Antony <amwaniki180@gmail.com>
2019-06-26 19:28:03 +0300

    articles

--- app/models.py
+++ app/models.py
@@ -9,3 +9,16 @@ class Source:
         self.url = url
         self.category = category
         self.country = country
+
+class Articles:
+    '''
+    all new articles
+    '''
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+
+        self.author = author
+        self.title =title
+        self.description = description
+        self.url = url
+        self.urlToImage =urlToImage
+        self.publishedAt = publishedAt
